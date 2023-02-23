6862b2f5875108331f7ac4d22b0e6681d66b9f36
Auston Bunsen <auston.bunsen@gmail.com>
2023-02-23 01:38:35 -0500

    About page coming along

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,20 @@
 class StaticController < ApplicationController
   layout 'website'
+  before_action :set_bg_color, except: [:home]
+  before_action :set_show_cta, only: [:home]
 
   def home
+  end
+
+  def about
+  end
+
+  private
+  def set_bg_color
+    @bg_color = 'bg-ninja-red'
+  end
+
+  def set_show_cta
+    @show_cta = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static#home"
+  get "/about", to: "static#about"
 end
